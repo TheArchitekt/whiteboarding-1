462ed8c0fb1b6bb1a37310e008f6ec51042c51ff
TheArchitekt <richardpennymon@gmail.com>
2022-11-10 09:02:22 -0500

    Add Questions

---
+++ practice.py
@@ -0,0 +1,14 @@
+# Write a function that takes in a list of numbers and prints out each number that is less than 10.
+
+# Write a function that takes in a list of words. For each word, the function should print a tuple of (first_letter_of_word, word).
+
+# Write a function that takes in a list of numbers. It should print every other number, starting with the number at index 0
+
+# Write a function that takes in a list and an item. It should return True if the list contains the item. Otherwise, return False.
+
+# Write a function that takes in a string and returns the length of that string. You cannot use the len function.
+
+# Write a function that takes in a sentence as a string. The function should print the length of each word in the sentence.
+#  Your sentence will not contain any punctuation.
+
+# Write a function that takes in a list of numbers and returns the largest number in the list. You cannot use the max function.
